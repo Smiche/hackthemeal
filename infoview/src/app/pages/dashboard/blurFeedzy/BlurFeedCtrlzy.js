1762e0f448aad8407f89739b36c515b01bec616e
Aleksandar Ivanov <smiche@smiche-Lenovo-G580>
2016-11-13 03:44:57 +0200

    .

--- infoview/src/app/pages/dashboard/blurFeedzy/BlurFeedCtrlzy.js
+++ infoview/src/app/pages/dashboard/blurFeedzy/BlurFeedCtrlzy.js
@@ -11,55 +11,60 @@
   /** @ngInject */
   function BlurFeedCtrlzy($scope, dataService) {
     dataService.getWeekPop().then(function (res) {
-      $scope.feed = res;
-     /*
-      $scope.feed = [
-        {
-          type: 'text-message',
-          author: 'Monday',
-          header: 'Posted new message',
-          text: 'Carrot balls',
-          time: 'Total times taken:',
-          ago: '584 times',
-          expanded: false,
-        }, {
-          type: 'text-message',
-          author: 'Tuesday',
-          header: 'Added new video',
-          text: 'Sausage backed with cheese',
-          time: 'Total times taken:',
-          ago: '1364 times',
-          expanded: false,
-        }, {
-          type: 'text-message',
-          author: 'Wednesday',
-          header: 'Added new video',
-          text: 'Grilled roast beef',
-          time: 'Total times taken:',
-          ago: '1427 times',
-          expanded: false,
-        }, {
-          type: 'text-message',
-          author: 'Thursday',
-          header: 'Added new video',
-          text: 'Carbonara',
-          time: 'Total times taken:',
-          ago: '1273 times',
-          expanded: false,
-        }, {
-          type: 'text-message',
-          author: 'Friday',
-          header: 'Added new video',
-          text: 'Stroganoff',
-          time: 'Total times taken:',
-          ago: '625 times',
-          expanded: false,
-        }
-      ];
+      var weekDays = ["", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+      for (var key in res) {
+        res[key].weekday = weekDays[res[key].day];
+      }
 
-      $scope.expandMessage = function (message) {
-        message.expanded = !message.expanded;
-      } */
+      $scope.feed = res;
+      /*
+       $scope.feed = [
+         {
+           type: 'text-message',
+           author: 'Monday',
+           header: 'Posted new message',
+           text: 'Carrot balls',
+           time: 'Total times taken:',
+           ago: '584 times',
+           expanded: false,
+         }, {
+           type: 'text-message',
+           author: 'Tuesday',
+           header: 'Added new video',
+           text: 'Sausage backed with cheese',
+           time: 'Total times taken:',
+           ago: '1364 times',
+           expanded: false,
+         }, {
+           type: 'text-message',
+           author: 'Wednesday',
+           header: 'Added new video',
+           text: 'Grilled roast beef',
+           time: 'Total times taken:',
+           ago: '1427 times',
+           expanded: false,
+         }, {
+           type: 'text-message',
+           author: 'Thursday',
+           header: 'Added new video',
+           text: 'Carbonara',
+           time: 'Total times taken:',
+           ago: '1273 times',
+           expanded: false,
+         }, {
+           type: 'text-message',
+           author: 'Friday',
+           header: 'Added new video',
+           text: 'Stroganoff',
+           time: 'Total times taken:',
+           ago: '625 times',
+           expanded: false,
+         }
+       ];
+ 
+       $scope.expandMessage = function (message) {
+         message.expanded = !message.expanded;
+       } */
     }).catch(function (err) {
       console.log(err);
     });
